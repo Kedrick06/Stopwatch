430c81e71579f4b3eb4c91c4d5cfbab7d15cf580
Kedrick06 <bfloblk@gmail.com>
2020-07-31 16:46:02 -0500

    second commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Stopwatch from './Components/Stopwatch'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Stopwatch />
+      </header>
+    </div>
+  );
+}
+
+export default App;

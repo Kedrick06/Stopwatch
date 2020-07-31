bdf98672da929b78095a3341fa7135ab686e0dfc
Kedrick06 <bfloblk@gmail.com>
2020-07-31 17:23:33 -0500

    fourth

--- src/Components/Stopwatch.js
+++ src/Components/Stopwatch.js
@@ -9,8 +9,9 @@ class Stopwatch extends Component {
         timerStart: 0,
         timerTime: 0
     };
-    /* Setting the Stopwatch timer */
-    startTimer = () => {
+    /* Setting the Stopwatch timer to turn on */
+    beginTimer = () => {
+
         this.setState({
             timerOn: true,
             timerTime: this.state.timerTime,
@@ -22,11 +23,12 @@ class Stopwatch extends Component {
             });
         }, 10);
     };
-    
+    /* Setting the Stopwatch timer to stop */
     stopTimer = () => {
         this.setState({ timerOn: false });
         clearInterval(this.timer);
     };
+    /* Setting the Stopwatch timer to turn on */
     resetTimer = () => {
         this.setState({
             timerStart: 0,
@@ -34,30 +36,35 @@ class Stopwatch extends Component {
         });
     };
     render() {
-        /**
-         * Formatting and Display
-         */
+
+    /*Rendering the display and calculating */
+
         const { timerTime } = this.state;
-        let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+        
         let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
         let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
         let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+
+
         return (
             <div className="Stopwatch">
-                <div className="Stopwatch-header">Stopwatch</div>
-                <div className="Stopwatch-display">
-                    {hours} : {minutes} : {seconds} : {centiseconds}
+                <div className="display">
+                    {hours} : {minutes} : {seconds}
+                    <br></br>
                     {this.state.timerOn === false && this.state.timerTime === 0 && (
-                        <button onClick={this.startTimer}>Start</button>
+                        <button className="button" onClick={this.beginTimer}>Start</button>
                     )}
+                    <br></br>
                     {this.state.timerOn === true && (
-                        <button onClick={this.stopTimer}>Stop</button>
+                        <button className="button" onClick={this.stopTimer}>Stop</button>
                     )}
+                    <br></br>
                     {this.state.timerOn === false && this.state.timerTime > 0 && (
-                        <button onClick={this.startTimer}>Resume</button>
+                        <button className="button" onClick={this.beginTimer}>Continue</button>
                     )}
+                    <br></br>
                     {this.state.timerOn === false && this.state.timerTime > 0 && (
-                        <button onClick={this.resetTimer}>Reset</button>
+                        <button className="button" onClick={this.resetTimer}>Reset</button>
                     )}
                 </div>
             </div>
